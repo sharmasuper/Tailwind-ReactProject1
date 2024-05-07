60db22dd9ff53b6f28fc67969471cde62fa6d1ff
sharmasuper <mohit6375sharma@gmail.com>
2024-05-08 01:45:14 +0530

    correct base add

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base :'/personal_website/',
+  base :"/Tailwind-ReactProject1/",                //jo directory hoti h vo hi likhtai h
   build:{
     outDir:"./docs"
   }
